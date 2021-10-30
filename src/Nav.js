40c2b5d38310278291f7ac57fa0c766cf632aad3
Damilola Ale <5752162-codenlyn@users.noreply.gitlab.com>
2021-10-30 12:32:19 +0100

    Change NAV size

--- src/Nav.js
+++ src/Nav.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <div className="flex justify-between p-10 text-3xl">
+    <div className="flex justify-between p-10 text-2xl">
       <li className="list-none uppercase font-bold tracking-widest">Israel</li>
       <li className="list-none uppercase font-bold tracking-widest">Project
-        <span className="text-4xl mx-2">/</span>
+        <span className="text-3xl mx-2">/</span>
         <span className="text-gray-600">others</span>
       </li>
       <li className="list-none uppercase font-bold text-gray-600 tracking-widest">About</li>
